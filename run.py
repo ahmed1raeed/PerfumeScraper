b0592f03741e0a7dd4a0c407fe76c64150d4eedc
ahmed1raeed <am2864701@gmail.com>
2026-07-16 00:33:00 +0300

    Refactor scraping process to iterate over multiple sites and update price comparison

--- run.py
+++ run.py
@@ -1,4 +1,23 @@
 from engine import scrape
-from sites.cozy import SITE
+from merge_brands import merge_brands
+from sites import SITES
 
-scrape(SITE)
+print(f"Found {len(SITES)} brands.\n")
+
+for site in SITES:
+
+    print("=" * 60)
+    print(f"Scraping {site['name']}")
+    print("=" * 60)
+
+    try:
+        scrape(site)
+
+    except Exception as e:
+        print(f"❌ {site['name']} -> {e}")
+
+print("\nUpdating Price Comparison...\n")
+
+merge_brands()
+
+print("\nFinished ✔")
